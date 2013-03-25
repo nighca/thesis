73fad9fa40caddae6e302c9848a2bd7f89ffe7bf
nighca <nighca@live.cn>
2013-03-25 08:30:33 +0800

    init gh-pages

---
+++ js/dataset.js
@@ -0,0 +1,182 @@
+var dataset = {
+  id: "artists",
+  title: "Artists",
+  theme: "orange",
+  // note that the lines below are now the preferred syntax
+  type: "basic",
+  options: {
+    items: [
+      {
+        "start" : "1399",
+        "end" : "1444-01-11",
+        "point" : {
+            "lat" : 43.7717,
+            "lon" : 11.2536
+         },
+        "title" : "Domenico Ghirlandaio"
+      },
+      {
+        "start" : "1402",
+        "end" : "1569",
+        "point" : {
+            "lat" : 43.8166666667,
+            "lon" : 10.7666666667
+         },
+        "title" : "Leonardo da Vinci"
+      },
+      {
+        "start" : "1475",
+        "end" : "1664",
+        "point" : {
+            "lat" : 43.6433,
+            "lon" : 11.9875
+         },
+        "title" : "Michelangelo"
+      }/*,
+      {
+        "start" : "1449",
+        "end" : "1694-01-11",
+        "point" : {
+            "lat" : 42.7717,
+            "lon" : 11.2536
+         },
+        "title" : "1"
+      },
+      {
+        "start" : "1352",
+        "end" : "1569",
+        "point" : {
+            "lat" : 41.8166666667,
+            "lon" : 10.7666666667
+         },
+        "title" : "2"
+      },
+      {
+        "start" : "1475",
+        "end" : "1560",
+        "point" : {
+            "lat" : 43.6433,
+            "lon" : 12.9875
+         },
+        "title" : "3"
+      },
+      {
+        "start" : "1275",
+        "end" : "1360",
+        "point" : {
+            "lat" : 45.6433,
+            "lon" : 12.9875
+         },
+        "title" : "7"
+      },
+      {
+        "start" : "1375",
+        "end" : "1560",
+        "point" : {
+            "lat" : 44.6433,
+            "lon" : 10.9875
+         },
+        "title" : "9"
+      },
+      {
+        "start" : "1275",
+        "end" : "1360",
+        "point" : {
+            "lat" : 40.6433,
+            "lon" : 8.9875
+         },
+        "title" : "9"
+      },
+      {
+        "start" : "1175",
+        "end" : "1260",
+        "point" : {
+            "lat" : 40.6433,
+            "lon" : 13.9875
+         },
+        "title" : "9"
+      },
+      {
+        "start" : "1452",
+        "end" : "1519",
+        "point" : {
+            "lat" : 43.8166666667,
+            "lon" : 10.7666666667
+         },
+        "title" : "Leonardo da Vinci"
+      },
+      {
+        "start" : "1235",
+        "end" : "1564",
+        "point" : {
+            "lat" : 43.6433,
+            "lon" : 11.9875
+         },
+        "title" : "Michelangelo"
+      },
+      {
+        "start" : "1449",
+        "end" : "1694-01-11",
+        "point" : {
+            "lat" : 42.7717,
+            "lon" : 11.2536
+         },
+        "title" : "1"
+      },
+      {
+        "start" : "1452",
+        "end" : "1519",
+        "point" : {
+            "lat" : 43.8166666667,
+            "lon" : 10.7666666667
+         },
+        "title" : "Leonardo da Vinci"
+      },
+      {
+        "start" : "1235",
+        "end" : "1564",
+        "point" : {
+            "lat" : 43.6433,
+            "lon" : 11.9875
+         },
+        "title" : "Michelangelo"
+      },
+      {
+        "start" : "1449",
+        "end" : "1694-01-11",
+        "point" : {
+            "lat" : 42.7717,
+            "lon" : 11.2536
+         },
+        "title" : "1"
+      },
+      {
+        "start" : "1452",
+        "end" : "1519",
+        "point" : {
+            "lat" : 43.8166666667,
+            "lon" : 10.7666666667
+         },
+        "title" : "Leonardo da Vinci"
+      },
+      {
+        "start" : "1235",
+        "end" : "1564",
+        "point" : {
+            "lat" : 43.6433,
+            "lon" : 11.9875
+         },
+        "title" : "Michelangelo"
+      },
+      {
+        "start" : "1449",
+        "end" : "1694-01-11",
+        "point" : {
+            "lat" : 42.7717,
+            "lon" : 11.2536
+         },
+        "title" : "1"
+      }*/
+    ]
+  }
+};
---
+++ js/map-op.js
@@ -0,0 +1,94 @@
+function placeMarker(location, icon) {
+  var marker = new google.maps.Marker({
+      position: location,
+      icon: icon || null,
+      map: map
+  });
+
+  return marker;  
+}
+
+function destroyOverLay(obj) {
+  if(obj) obj.setMap(null);
+}
+
+
+function drawPolygon(points){
+  var polygon = new google.maps.Polygon({
+    paths: points,
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2,
+
+    fillColor: "#FF0000",
+    fillOpacity: 0.35,
+
+    editable: true
+  });
+
+  polygon.setMap(map);
+  return polygon;
+}
+
+function enablePolygonSelect(afterSelect) {
+  var overLays = {};
+
+  var listeners = {};
+
+  overLays["polygonMarkers"] = [];
+
+
+  
+  listeners["map"] = google.maps.event.addListener(map, 'click', function(event) {
+    if(overLays["polygon"]){ //to release certain polygon
+      destroyOverLay(overLays["polygon"]);
+      overLays["polygon"] = null;
+    }
+
+    var marker = placeMarker(event.latLng, polygonMarkerIcon);
+    if(overLays["polygonMarkers"].push(marker)==1){
+      marker.setIcon(polygonStartMarkerIcon);
+      listeners["marker"] = google.maps.event.addListener(marker, 'click', function(event) {
+        var points = [];
+        var polygonMarker;
+
+        while(polygonMarker = overLays["polygonMarkers"].pop()){
+          points.push(polygonMarker.position);
+          destroyOverLay(polygonMarker);
+        }
+
+        overLays["polygon"] = drawPolygon(points);
+
+        afterSelect && afterSelect();
+
+        overLays["polygonMarkers"] = [];
+      });
+    }
+  });
+
+  return {
+    getOverLays: function(){
+      return overLays;
+    },
+    getListeners: function(){
+      return listeners;
+    }
+  };
+}
+
+function disablePolygonSelect(e){
+  var listeners = e.getListeners();
+  for(var i in listeners){
+    google.maps.event.removeListener(listeners[i]);
+  }
+
+  var overLays = e.getOverLays();
+  destroyOverLay(overLays["polygon"]);
+
+  var polygonMarkers = overLays["polygonMarkers"];
+  for (var i = polygonMarkers.length - 1; i >= 0; i--) {
+    destroyOverLay(polygonMarkers[i]);
+  };
+
+  return null;
+}
